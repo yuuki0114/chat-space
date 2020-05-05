a099c4670fbc456ed9695f6357a36bf496073e85
yuuki0114 <y.no0114@gmail.com>
2020-05-05 14:30:32 +0900

    final-adjustment2

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -23,8 +23,9 @@ $(function() {
     var html = `
     <div class="chat-group-user clearfix js-chat-member" id="${id}">
       <p class="chat-group-user__name">${name}</p>
-      <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn>削除</a>
-    </div> `;
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+
     $(".js-add-user").append(html);
   }
 
